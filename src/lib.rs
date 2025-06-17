5e3361967c2d59ab816d8c0f8f194f27610c1348
vincent.goguyer <vincent.goguyer@gmail.com>
2025-06-17 06:05:50 +0000

    feat: initializing a test structure

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub fn compression_file(a: i32) -> i32{
+    return a*2;
+}
---
+++ tests/integration.rs
@@ -0,0 +1,8 @@
+use silver_memory::compression_file;
+
+#[test]
+fn it_adds_two() {
+    let result = silver_memory::compression_file(2);
+
+    assert_eq!(result, 4);
+}
