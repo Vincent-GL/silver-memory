438f1683e53de18db208a0adec0232e50aef5e5f
vincent.goguyer <vincent.goguyer@gmail.com>
2025-06-18 20:16:03 +0000

    feat: cleaning up

--- src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub fn compression_file(a: i32) -> i32{
-    return a*2;
-}
--- tests/integration.rs
+++
@@ -1,8 +0,0 @@
-use silver_memory::compression_file;
-
-#[test]
-fn it_adds_two() {
-    let result = silver_memory::compression_file(2);
-
-    assert_eq!(result, 4);
-}
